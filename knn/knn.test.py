a05b418845cd06547f21ad7cff8f87417f1af628
Wellington Oliveira <well.oliveira.snt@gmail.com>
2017-10-30 17:32:26 -0300

    GridSearch and Random Search

--- knn/knn.test.py
+++ knn/knn.test.py
@@ -2,6 +2,8 @@ import csv
 
 import sys
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
 class DataCollector:
@@ -9,7 +11,7 @@ class DataCollector:
         print("Started")
 
     def getEntrada(self, nome_ficheiro):
-        vetorEntrada = []
+        vetor_entrada = []
 
         with open(nome_ficheiro, 'r') as ficheiro:
             reader = csv.reader(ficheiro)
@@ -19,33 +21,72 @@ class DataCollector:
                     for coluna in linha[:-1]:
                         novaLinha.append(float(coluna))
                     novaLinha.append(linha[-1])
-                    vetorEntrada.append(novaLinha)
+                    vetor_entrada.append(novaLinha)
             except csv.Error as e:
                 sys.exit('ficheiro %s, linha %d: %s' % (nome_ficheiro, reader.line_num, e))
-        return self.getVetorXySeparado(vetorEntrada)
+        return self.getVetorXySeparado(vetor_entrada)
 
-    def getVetorXySeparado(self, vetorEntrada):
+    def getVetorXySeparado(self, vetor_entrada):
         x = []
         y = []
 
-        for linha in vetorEntrada:
-            linhaXAux = []
+        for linha in vetor_entrada:
+            linha_x_aux = []
             for coluna in linha[:-1]:
-                clounaTemp = coluna
-                linhaXAux.append(clounaTemp)
-            x.append(linhaXAux)
+                clouna_temp = coluna
+                linha_x_aux.append(clouna_temp)
+            x.append(linha_x_aux)
             y.append(linha[-1])
+
         return x, y
 
 
-if __name__ == "__main__":
+def main():
     data = DataCollector()
-    X, y = data.getEntrada('datasets/iris.data')
+    data_sets = ['iris', 'ionosphere']
 
-    neigh = KNeighborsClassifier(n_neighbors=3)
+    k_range = list(range(1, 31))
+    weight_options = ['uniform', 'distance']
+    param_grid_in = dict(n_neighbors=k_range, weights=weight_options)
 
-    neigh.fit(X, y)
+    print('----------------------------------------------------')
+    for dataset in data_sets:
 
-    print(neigh.predict([[5.1, 3.5, 1.4, 0.2]]))
+        print('Dataset:' + dataset)
 
-#    print(neigh.predict_proba(['Iris-setosa']))
+        X, y = data.getEntrada('datasets/' + dataset + '.data')
+
+        knn = KNeighborsClassifier(n_neighbors=10, weights='distance', algorithm='ball_tree')
+
+        search_methods = [('Grid', GridSearchCV(knn, param_grid_in, cv=10, scoring='accuracy')),
+                          ('Random',
+                           RandomizedSearchCV(knn, param_grid_in, cv=10, scoring='accuracy', n_iter=30, random_state=5))]
+
+        for name, search_method in search_methods:
+            print('----------------------------------------------------')
+            print('Search method: ' + name)
+            search_method.fit(X, y)
+            print("Best score:")
+            print(search_method.best_score_)
+            print()
+
+            print("Best params:")
+            print(search_method.best_params_)
+            print()
+
+            # print_scores(search_method)
+
+
+def print_scores(search_method):
+    print("Lista de scores:")
+    means = search_method.cv_results_['mean_test_score']
+    stds = search_method.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, search_method.cv_results_['params']):
+        print("%0.6f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print('----------------------------------------------------')
+    print()
+
+
+if __name__ == "__main__":
+    main()
